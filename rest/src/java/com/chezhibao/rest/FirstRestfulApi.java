719ae694d6e9cc5e4f386e5de47bb12cd9c56415
Arshle <329756669@qq.com>
2018-03-25 00:34:25 +0800

    测试git

--- rest/src/java/com/chezhibao/rest/FirstRestfulApi.java
+++
@@ -1,7 +0,0 @@
-package com.chezhibao.rest;
-
-public class FirstRestfulApi {
-    public static void main(String[] args) {
-        String s = "";
-    }
-}
---
+++ rest/src/java/com/chezhibao/rest/RestTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C), 2018-2020,中间件开发工程师
+ * FileName: RestTest.java
+ * Author:   Arshle
+ * Date:     2018年03月25日
+ * Description: 测试
+ */
+package com.chezhibao.rest;
+
+/**
+ * 〈测试〉<br>
+ * 〈测试〉
+ *
+ * @author Arshle
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本]（可选）
+ */
+public class RestTest {
+}
